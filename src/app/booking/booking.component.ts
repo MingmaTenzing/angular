5ed1795aa8ed87183cd922e2109bd61b7096cbbd
MingmaTenzing <pasangsherpa689@gmail.com>
2023-10-19 13:30:47 +0800

    need to work more on forms

--- src/app/booking/booking.component.ts
+++ src/app/booking/booking.component.ts
@@ -1,58 +1,58 @@
 import { Component, OnInit } from '@angular/core';
-import { FormArray, FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-booking',
   templateUrl: './booking.component.html',
   styleUrls: ['./booking.component.css'],
- 
 })
-export class BookingComponent implements OnInit{
-  panelOpenState:boolean = false;
+export class BookingComponent implements OnInit {
+  panelOpenState: boolean = false;
   bookingForm!: FormGroup;
 
-
-  constructor( private fb:FormBuilder) {}
+  constructor(private fb: FormBuilder) {}
 
   get guests() {
     return this.bookingForm.get('guests') as FormArray;
-
   }
 
   ngOnInit(): void {
-      this.bookingForm = this.fb.group({
-        roomId:new FormControl(''),
-        guestEmail: [''],
-        checkinDate:[''],
-        checkoutDate:[''],
-        bookingStatus: [''],
-        bookingAmount: [''],
-        bookingDate:[''],
-        mobileNumber: [''],
-       
-        address: this.fb.group({
-          addressline1: [''],
+    this.bookingForm = this.fb.group({
+      roomId: new FormControl(''),
+      guestEmail: ['', Validators.required, Validators.email],
+      checkinDate: [''],
+      checkoutDate: [''],
+      bookingStatus: [''],
+      bookingAmount: [''],
+      bookingDate: [''],
+      mobileNumber: [''],
+
+      address: this.fb.group({
+        addressline1: [''],
         addressLine2: [''],
         guestCity: [''],
         guestState: [''],
         guestCountry: [''],
         guestZipCode: [''],
-        }),
-        guestCount: [''],
-        guests: this.fb.array([ 
-          this.fb.group({ guestName: [''], age: new FormControl("")})
-        ])
-      })
+      }),
+      guestCount: [''],
+      guests: this.fb.array([this.addGuestControl()]),
+    });
   }
 
   addBooking() {
-    console.log(this.bookingForm.value)
+    console.log(this.bookingForm.value);
+    this.bookingForm.reset()
   }
 
   addGuest() {
-    this.guests.push(this.fb.group({guestName:[''], age: new FormControl("")}))
-   
-
+    this.guests.push(this.addGuestControl());
   }
 
+  addGuestControl() {
+    return this.fb.group({ guestName: [''], age: new FormControl('') });
+  }
+  removeGuest(i: number) {
+    this.guests.removeAt(i);
+  }
 }
